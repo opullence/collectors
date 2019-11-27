1a7d70b53510d16cae3b3cc9504402d817af441b
henry <henry.fumery@epitech.eu>
2019-11-27 15:32:36 +0100

    virus total collector init

---
+++ opulence/collectors/collectors/virus_total.py
@@ -0,0 +1,25 @@
+from opulence.facts.file import File
+from opulence.common.patterns import Composite
+
+from ..bases import BaseCollector
+
+
+class VirusTotal(BaseCollector):
+    ###############
+    # Plugin attributes
+    ###############
+    _name_ = "virus total collector"
+    _description_ = "return info on file from his hash"
+    _author_ = "Henry"
+    _version_ = 1
+
+    ###############
+    # Module attributes
+    ###############
+    _allowed_input_ = File
+
+    def launch(self, facts):
+        return File(
+            filename = "file name from virus total",
+            hash = "fffffffffff"
+        )
